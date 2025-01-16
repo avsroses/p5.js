cdf99099cc64968fe4a3d5f333dd6da9f2c1d511
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2025-01-16 12:27:19 +0000

    add multiple disks

--- p5.js/Disk.js
+++ p5.js/Disk.js
@@ -4,28 +4,34 @@ class Disk {
     d;
     vx;
     vy;
-    constructor(x,y,d,vx,vy){
+    colour;
+    constructor(x, y, d) {
         this.x = x;
         this.y = y;
         this.d = d;
-        this.vx = vx;
-        this.vy = vy;
+        this.vx = random(-10, 10);
+        this.vy = random(-10, 10);
+        this.colour = color(random(255), random(255), random(255));
     }
-    draw(){
+
+    draw() {
+        fill(this.colour);
+        strokeWeight(0);
         circle(this.x, this.y, this.d);
     }
+
     update() {
-    this.x += this.vx;
-    this.y += this.vy;
-    //radius
-    let r = this.d / 2;
-    //won't go off x
-    if (this.x < 0 + r || this.x > width - r) {
-      this.vx = -this.vx;
-    }
-    //won't go off y
-    if (this.y < 0 + r || this.y > height - r) {
-      this.vy = -this.vy;
-    }
+        this.x += this.vx;
+        this.y += this.vy;
+        //radius
+        let r = this.d / 2;
+        //won't go off x
+        if (this.x < 0 + r || this.x > width - r) {
+            this.vx = -this.vx;
+        }
+        //won't go off y
+        if (this.y < 0 + r || this.y > height - r) {
+            this.vy = -this.vy;
+        }
     }
 }
--- p5.js/sketch.js
+++ p5.js/sketch.js
@@ -1,12 +1,32 @@
 let disk;
+let disk2;
+let disk3;
+let disk4;
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
-  disk = new Disk(width/2, height/2, 50, 5, 5)
+
+  disk = new Disk(width/2, height/2, 150);
+
+  disk2 = new Disk(width/2, height/2, 150);
+
+  disk3 = new Disk(width/2, height/2, 150);
+
+  disk4 = new Disk(width/2, height/2, 150);
 }
 
 function draw() {
-  background(220);
+  background(220, 10);
+  //disk1
   disk.draw();
   disk.update();
+  //disk2
+  disk2.draw();
+  disk2.update();
+  //disk3
+  disk3.draw();
+  disk3.update();
+  //disk4
+  disk4.draw();
+  disk4.update();
 }

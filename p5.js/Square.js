7ce401e281ca44fb3a443914b70401f538ab6f0e
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2025-01-16 14:26:59 +0000

    optimise code with arrays

--- p5.js/Square.js
+++ p5.js/Square.js
@@ -1,12 +1,7 @@
 class Square extends Shape{
 
     constructor(x, y, d) {
-        this.x = x;
-        this.y = y;
-        this.d = d;
-        this.vx = random(-10, 10);
-        this.vy = random(-10, 10);
-        this.colour = color(random(255), random(255), random(255));
+        super(x,y,d)
         this.rotation = 10;
         angleMode(DEGREES);
         rectMode(CENTER);
@@ -23,18 +18,7 @@ class Square extends Shape{
     }
 
     update() {
-        this.x += this.vx;
-        this.y += this.vy;
-        //radius
-        let r = this.d / 2;
-        //won't go off x
-        if (this.x < 0 + r || this.x > width - r) {
-            this.vx = -this.vx;
-        }
-        //won't go off y
-        if (this.y < 0 + r || this.y > height - r) {
-            this.vy = -this.vy;
-        }
+        super.update();
         this.rotation+=5;
     }
 }
--- p5.js/sketch.js
+++ p5.js/sketch.js
@@ -1,55 +1,23 @@
 //global variables
-let disk;
-let disk2;
-let disk3;
-let disk4;
-
-let mySquare;
-let mySquare2;
-let mySquare3;
-let mySquare4;
+let shapes = [];
 
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
   //each Disk needs three values for the contrsuctor function
-  disk = new Disk(width/2, height/2, 150);
-  disk2 = new Disk(width/2, height/2, 150);
-  disk3 = new Disk(width/2, height/2, 150);
-  disk4 = new Disk(width/2, height/2, 150);
-
-  mySquare = new Square(width/2, height/2, 150);
-  mySquare2 = new Square(width/2, height/2, 150);
-  mySquare3 = new Square(width/2, height/2, 150);
-  mySquare4 = new Square(width/2, height/2, 150);
+  for (let i = 0; i < 5; i++) {
+    shapes.push(new Disk(width / 2, height / 2, 150));
+    shapes.push(new Square(width / 2, height / 2, 150));
+  }
 
 }
 
 function draw() {
   background(220, 15);
-  //disk1
-  disk.draw();
-  disk.update();
-  //disk2
-  disk2.draw();
-  disk2.update();
-  //disk3
-  disk3.draw();
-  disk3.update();
-  //disk4
-  disk4.draw();
-  disk4.update();
 
-  //square1
-  mySquare.draw();
-  mySquare.update();
-  //square2
-  mySquare2.draw();
-  mySquare2.update();
-  //square3
-  mySquare3.draw();
-  mySquare3.update();
-  //square4
-  mySquare4.draw();
-  mySquare4.update();
+  for (let i = 0; i < shapes.length; i++) {
+    shapes[i].update();
+    shapes[i].draw();
+  }
+
 }

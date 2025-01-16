fdfa67e2865ff344fa38ce5088d673f6237a016e
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2025-01-16 11:22:06 +0000

    move circle

--- p5.js/sketch.js
+++ p5.js/sketch.js
@@ -1,7 +1,25 @@
+//global variables
+let x;
+let y;
+let d = 50;
+let vx = 5;
+let vy = 5;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(innerWidth, innerHeight);
+  x = width/2;
+  y = height/2;
 }
 
 function draw() {
   background(220);
+  //logic
+  x += vx;
+  y += vy;
+  let r = d/2;
+  if(x < 0 + r || x > width - r) {
+    
+  }
+  //draw
+  circle(x, y, d);
 }

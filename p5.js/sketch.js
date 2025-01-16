7b0540d39ce0e9f1c6fd12a91172a9651537fdc9
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2025-01-16 11:38:51 +0000

    using objects

--- p5.js/sketch.js
+++ p5.js/sketch.js
@@ -1,31 +1,27 @@
-//global variables
-let x;
-let y;
-let d = 50;
-let vx = 5;
-let vy = 5;
+//sketch-topdown using objects
+let disk;
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
-  x = width/2;
-  y = height/2;
+  //object - (variable name : value , )
+  disk = {x:width/2, y:height/2, d:40, vx:5, vy:5}
 }
 
 function draw() {
   background(220);
   //logic
-  x += vx;
-  y += vy;
+  disk.x += disk.vx;
+  disk.y += disk.vy;
   //radius
-  let r = d/2;
+  let r = disk.d/2;
   //won't go off x
-  if(x < 0 + r || x > width - r) {
-    vx = -vx;
+  if(disk.x < 0 + r || disk.x > width - r) {
+    disk.vx = -disk.vx;
   }
   //won't go off y
-  if(y < 0 + r || y > height - r) {
-    vy = -vy;
+  if(disk.y < 0 + r || disk.y > height - r) {
+    disk.vy = -disk.vy;
   }
   //draw
-  circle(x, y, d);
+  circle(disk.x, disk.y, disk.d);
 }

6ddd50ef792f19d4a9b7f57eb508b4c2de749a4d
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2025-01-16 11:24:21 +0000

    circle bounce off walls

--- p5.js/sketch.js
+++ p5.js/sketch.js
@@ -18,7 +18,10 @@ function draw() {
   y += vy;
   let r = d/2;
   if(x < 0 + r || x > width - r) {
-    
+    vx = -vx;
+  }
+  if(y < 0 + r || y > height - r) {
+    vy = -vy;
   }
   //draw
   circle(x, y, d);
